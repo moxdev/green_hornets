9da763e810376571c82344d86faf74dab6d923be
Stan Szeto <stan@Stan-Szetos-MacBook-Pro-2.local>
2014-09-26 13:36:48 -0700

    Updated view solution

--- view.rb
+++ view.rb
@@ -1,57 +1,48 @@
 # Flash Card View
-
 class View
+  attr_accessor :user_input
 
-  attr_reader :user_input
-
-  def self.display_welcome
-
+  def self.welcome_message
     puts "Welcome to the OG Ruby Flash Cards for Dummies!"
+    puts "__________________________________"
+
     `say "Welcome to the OG Ruby Flash Cards for Dummies!"`
-
-
     puts "To play, hit enter to start and just enter the correct term for each definition"
     puts "Type 'exit' to quit the program."
     puts "Yo. Yo. You ready?"
     `say "Yo. Yo. You ready?"`
 
-    get_user_input
-
-    if @user_input.length == 0   # user is ready to play game
+    @user_input = gets.chomp
+    if @user_input.length == 0
       puts "Let's get this started"
       puts "Here is your first question"
-      Model.pick_question
-    elsif @user_input == "exit"
-      thanks_goodbye
     else
-      display_welcome
+      welcome_message
     end
+  end
 
+
+  def self.display_definition_to_user(card)
+    puts card.definition
   end
 
   def self.get_user_input
     @user_input = gets.chomp
   end
 
-  def self.get_question
-    Model.pick_question
-  end
-
-  def self.display_congrats
+  def self.congratulations_message
     puts "Great job! Ready for next question?"
   end
 
-  def self.thanks_goodbye
-    puts "Thanks for playing!  You are on the path to become a Ruby ninja!"
-    `say "Thanks for playing!  You are on the path to become a Ruby ninja!"`
-  end
-
-  def self.try_again
+  def self.try_again_message
     puts "Common, focus my son.  You can do better.  Superego is talking to you."
     `say "Common, focus my son.  You can do better.  Superego is talking to you."`
   end
 
+
+  def self.display_exit_message
+    puts "Thanks for playing!  You are on the path to become a Ruby ninja!"
+    `say "Thanks for playing!  You are on the path to become a Ruby ninja!"`
+  end
 end
 
-# Driver code
-View.display_welcome
