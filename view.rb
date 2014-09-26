898f3e8c277775e39cab1373863f0d96a5e4c644
Mox Biggs <Mox@Moxs-MacBook-Pro-2.local>
2014-09-26 16:17:50 -0700

    view images

--- view.rb
+++ view.rb
@@ -25,7 +25,7 @@ class View
 
   def self.display_definition_to_user(card)
     puts card.definition
-    `say -v "Bad News" "Don't be nervous. Everybody looking at you so they will finally realize how inept you are!"`    
+    `say -v "Alex" "Don't be nervous. You are already the dumbest person in the room so it can't get any worse!"`
   end
 
   def self.get_user_input
@@ -34,18 +34,107 @@ class View
 
   def self.congratulations_message
     puts "Great job! Ready for next question? Hit enter.".green
-    `say -v "Good News" "Great job! Ready for next question? Hit enter."`
+    `say -v "Alex" "Great job! Ready for next question? Hit enter."`
+
+    puts <<-BOSS
+      ,-------.
+    ,'         `.
+    : You A Boss :
+    :   Son!     :
+    '.         ,'
+      `._____,'
+          ||
+        _,''--.    _____
+       (/ __   `._|
+      ((_/_)\     |
+       (____)`.___|
+        (___)____.|_____
+          ||         SSt
+        _\||/_
+
+            _
+           /(|
+          (  :
+         __\  \  _____
+       (____)  `|
+      (____)|   |
+       (____).__|
+        (___)__.|_____
+                   SSt
+ BOSS
+
   end
 
   def self.try_again_message
-    puts "Come on, focus my son.  You can do better.  Superego is talking to you. Guess again.".red
-    `say -v "Bad News" "Come on, focus my son.  You can do better.  Superego is talking to you. Guess again."`
+    puts "Come on, focus my son.  You can do better.  Superego is talking to you. Guess again rookie.".red
+    `say -v "Alex" "Come on, focus my son.  You can do better.  Superego is talking to you. Guess again rookie."`
+
+    puts "┌∩┐(◣_◢)┌∩┐"
+    puts "╭∩╮(Ο_Ο)╭∩╮"
+    puts "╭∩╮（︶︿︶）╭∩╮"
+
   end
 
 
   def self.display_exit_message
     puts "Thanks for playing!  You are on the path to become a Ruby ninja!"
-    `say -v "Bad News" "Bye, bye. Eveybody knows now how stupid you are!"`
+    `say -v "Alex" "Bye, bye. Now everyone knows how stupid you are!"`
+
+    puts <<-SKULL
+                           ...----....
+                         ..-:"''         ''"-..
+                      .-'                      '-.
+                    .'              .     .       '.
+                  .'   .          .    .      .    .''.
+                .'  .    .       .   .   .     .   . ..:.
+              .' .   . .  .       .   .   ..  .   . ....::.
+             ..   .   .      .  .    .     .  ..  . ....:IA.
+            .:  .   .    .    .  .  .    .. .  .. .. ....:IA.
+           .: .   .   ..   .    .     . . .. . ... ....:.:VHA.
+           '..  .  .. .   .       .  . .. . .. . .....:.::IHHB.
+          .:. .  . .  . .   .  .  . . . ...:.:... .......:HIHMM.
+         .:.... .   . ."::"'.. .   .  . .:.:.:II;,. .. ..:IHIMMA
+         ':.:..  ..::IHHHHHI::. . .  ...:.::::.,,,. . ....VIMMHM
+        .:::I. .AHHHHHHHHHHAI::. .:...,:IIHHHHHHMMMHHL:. . VMMMM
+       .:.:V.:IVHHHHHHHMHMHHH::..:" .:HIHHHHHHHHHHHHHMHHA. .VMMM.
+       :..V.:IVHHHHHMMHHHHHHHB... . .:VPHHMHHHMMHHHHHHHHHAI.:VMMI
+       ::V..:VIHHHHHHMMMHHHHHH. .   .I":IIMHHMMHHHHHHHHHHHAPI:WMM
+       ::". .:.HHHHHHHHMMHHHHHI.  . .:..I:MHMMHHHHHHHHHMHV:':H:WM
+       :: . :.::IIHHHHHHMMHHHHV  .ABA.:.:IMHMHMMMHMHHHHV:'. .IHWW
+       '.  ..:..:.:IHHHHHMMHV" .AVMHMA.:.'VHMMMMHHHHHV:' .  :IHWV
+        :.  .:...:".:.:TPP"   .AVMMHMMA.:. "VMMHHHP.:... .. :IVAI
+       .:.   '... .:"'   .   ..HMMMHMMMA::. ."VHHI:::....  .:IHW'
+       ...  .  . ..:IIPPIH: ..HMMMI.MMMV:I:.  .:ILLH:.. ...:I:IM
+     : .   .'"' .:.V". .. .  :HMMM:IMMMI::I. ..:HHIIPPHI::'.P:HM.
+     :.  .  .  .. ..:.. .    :AMMM IMMMM..:...:IV":T::I::.".:IHIMA
+     'V:.. .. . .. .  .  .   'VMMV..VMMV :....:V:.:..:....::IHHHMH
+       "IHH:.II:.. .:. .  . . . " :HB"" . . ..PI:.::.:::..:IHHMMV"
+        :IP""HHII:.  .  .    . . .'V:. . . ..:IH:.:.::IHIHHMMMMM"
+        :V:. VIMA:I..  .     .  . .. . .  .:.I:I:..:IHHHHMMHHMMM
+        :"VI:.VWMA::. .:      .   .. .:. ..:.I::.:IVHHHMMMHMMMMI
+        :."VIIHHMMA:.  .   .   .:  .:.. . .:.II:I:AMMMMMMHMMMMMI
+        :..VIHIHMMMI...::.,:.,:!"I:!"I!"I!"V:AI:VAMMMMMMHMMMMMM'
+        ':.:HIHIMHHA:"!!"I.:AXXXVVXXXXXXXA:."HPHIMMMMHHMHMMMMMV
+          V:H:I:MA:W'I :AXXXIXII:IIIISSSSSSXXA.I.VMMMHMHMMMMMM
+            'I::IVA ASSSSXSSSSBBSBMBSSSSSSBBMMMBS.VVMMHIMM'"'
+             I:: VPAIMSSSSSSSSSBSSSMMBSSSBBMMMMXXI:MMHIMMI
+            .I::. "H:XIIXBBMMMMMMMMMMMMMMMMMBXIXXMMPHIIMM'
+            :::I.  ':XSSXXIIIIXSSBMBSSXXXIIIXXSMMAMI:.IMM
+            :::I:.  .VSSSSSISISISSSBII:ISSSSBMMB:MI:..:MM
+            ::.I:.  ':"SSSSSSSISISSXIIXSSSSBMMB:AHI:..MMM.
+            ::.I:. . ..:"BBSSSSSSSSSSSSBBBMMMB:AHHI::.HMMI
+            :..::.  . ..::":BBBBBSSBBBMMMB:MMMMHHII::IHHMI
+            ':.I:... ....:IHHHHHMMMMMMMMMMMMMMMHHIIIIHMMV"
+              "V:. ..:...:.IHHHMMMMMMMMMMMMMMMMHHHMHHMHP'
+               ':. .:::.:.::III::IHHHHMMMMMHMHMMHHHHM"
+                 "::....::.:::..:..::IIIIIHHHHMMMHHMV"
+                   "::.::.. .. .  ...:::IIHHMMMMHMV"
+                     "V::... . .I::IHHMMV"'
+                       '"VHVHHHAHHHHMMV:"'
+SKULL
+
   end
 end
+
+
 
